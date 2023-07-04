0381d758530701a9bec21c8bc33939c0e477cd39
Nabila <134735273+Kouraik-Nabila@users.noreply.github.com>
2023-07-04 16:09:33 +0200

    updated

--- script.js
+++ script.js
@@ -49,3 +49,15 @@ function activeWork(){
 }
 
 linkWork.forEach(l=> l.addEventListener('click', activeWork))
+
+// swiper
+
+var swiperTestimonial = new Swiper('.testimonial__container', {
+    spaceBetween: 24,
+    loop:true,
+    grabCursor:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+ });
